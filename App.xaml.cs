5c413729bb950169fc9f88d1e5865c7d08b1ba14
Jerry-Z07 <163709829+Jerry-Z07@users.noreply.github.com>
2025-12-25 23:13:40 +0800

    refactor(字体管理): 重构字体加载方式并优化资源引用
    - 将字体资源从静态引用改为动态加载
    - 在App.xaml.cs中添加字体加载逻辑
    - 更新项目文件中的字体资源定义
    - 统一使用DynamicResource引用字体

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Media;
+using System.IO;
 
 namespace WindBoard;
 
@@ -7,5 +9,46 @@ namespace WindBoard;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        LoadFonts();
+    }
+
+    private void LoadFonts()
+    {
+        try
+        {
+            var fontsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "resources", "fonts");
+            if (!Directory.Exists(fontsDirectory))
+            {
+                fontsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Fonts");
+            }
+
+            if (Directory.Exists(fontsDirectory))
+            {
+                var fontFiles = Directory.GetFiles(fontsDirectory, "*.ttf");
+                foreach (var fontFile in fontFiles)
+                {
+                    try
+                    {
+                        var fontFamily = new FontFamily(new Uri(fontFile), "./#" + Path.GetFileNameWithoutExtension(fontFile));
+                        Resources[Path.GetFileNameWithoutExtension(fontFile) + "FontFamily"] = fontFamily;
+                        
+                        if (Path.GetFileNameWithoutExtension(fontFile).StartsWith("MiSans"))
+                        {
+                            Resources["MiSansFontFamily"] = fontFamily;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+        catch
+        {
+        }
+    }
 }
 
